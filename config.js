a89585b578cdde2378fcaf13d6d3f9c797017fc5
yamamoto20031117-hash <yamamoto20031117@gmail.com>
2026-04-19 08:23:29 +0900

    Clean up config.js by removing unnecessary comments
    Removed comments related to Firebase and EmailJS settings.

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 /* =========================================================
    ピロロ病院 - 設定ファイル
-   =========================================================
-   Firebase と EmailJS の公開設定。
-   これらはブラウザから参照される「公開キー」です。
-   Firestoreのセキュリティルールで保護されている前提です。
    ========================================================= */
 
 window.PIRORO_CONFIG = {
 
-  // ------- Firebase 設定 -------
   firebase: {
     apiKey: "AIzaSyBzRLLORU8F3M0aqLwBZYcHLsecYncymOc",
     authDomain: "piroro-hospital.firebaseapp.com",
@@ -18,14 +13,12 @@ window.PIRORO_CONFIG = {
     appId: "1:77607584358:web:b61e0bbd949f201a7763a7"
   },
 
-  // ------- EmailJS 設定 -------
   emailjs: {
     serviceId:  "service_t7rl1ol",
     templateId: "template_tizzs09",
     publicKey:  "S1Ab1dYzeb9PcZWub"
   },
 
-  // ------- 病院情報（メール本文などで使用） -------
   hospital: {
     name: "ピロロ病院",
     address: "",
