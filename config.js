144636899e94cf4cd96bef30e799a2bc18f39222
yamamoto20031117-hash <yamamoto20031117@gmail.com>
2026-04-19 08:23:07 +0900

    Update Firebase and EmailJS settings in config.js
    Updated Firebase and EmailJS configuration details.

--- config.js
+++ config.js
@@ -1,37 +1,28 @@
 /* =========================================================
    ピロロ病院 - 設定ファイル
    =========================================================
-   このファイルに Firebase と EmailJS の情報を貼り付けてください。
-
-   ■ 手順
-   1. Firebase Console でプロジェクトを作り、ウェブアプリ設定から
-      firebaseConfig オブジェクトをコピーして下に貼り付ける。
-   2. EmailJS Dashboard で Service / Template を作成し、
-      それぞれの ID と Public Key を下に貼り付ける。
-
-   ※ これらはブラウザから参照される「公開キー」です。
-     ただしFirestoreのセキュリティルールは必ず設定してください。
+   Firebase と EmailJS の公開設定。
+   これらはブラウザから参照される「公開キー」です。
+   Firestoreのセキュリティルールで保護されている前提です。
    ========================================================= */
 
 window.PIRORO_CONFIG = {
 
   // ------- Firebase 設定 -------
-  // Firebase Console > プロジェクト設定 > ウェブアプリの設定 に表示される内容
   firebase: {
-    apiKey: "ここにAPIキーを貼り付け",
-    authDomain: "xxxxx.firebaseapp.com",
-    projectId: "xxxxx",
-    storageBucket: "xxxxx.appspot.com",
-    messagingSenderId: "xxxxx",
-    appId: "xxxxx"
+    apiKey: "AIzaSyBzRLLORU8F3M0aqLwBZYcHLsecYncymOc",
+    authDomain: "piroro-hospital.firebaseapp.com",
+    projectId: "piroro-hospital",
+    storageBucket: "piroro-hospital.firebasestorage.app",
+    messagingSenderId: "77607584358",
+    appId: "1:77607584358:web:b61e0bbd949f201a7763a7"
   },
 
   // ------- EmailJS 設定 -------
-  // EmailJS Dashboard にある Service ID / Template ID / Public Key
   emailjs: {
-    serviceId:  "service_xxxxxxx",
-    templateId: "template_xxxxxxx",
-    publicKey:  "xxxxxxxxxxxxxxxx"
+    serviceId:  "service_t7rl1ol",
+    templateId: "template_tizzs09",
+    publicKey:  "S1Ab1dYzeb9PcZWub"
   },
 
   // ------- 病院情報（メール本文などで使用） -------
